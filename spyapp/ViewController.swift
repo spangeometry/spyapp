9fab7b8b43061b8118c3c80de3565c98ddc0d5a8
spangeometry <serioussamix@gmail.com>
2018-09-16 16:50:09 -0700

    Initial commit.

--- spyapp/ViewController.swift
+++ spyapp/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var cipherControl: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var secretField: UITextField!
+    @IBOutlet weak var encodeButton: UIButton!
+    @IBOutlet weak var decodeButton: UIButton!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var cipherOutputLabel: UILabel!
+    
+    var cipherSelected: Int = 0
+    
+    @IBAction func cipherSelected(_ sender: Any) {
+        switch cipherControl.selectedSegmentIndex {
+        case 0:
+            outputLabel.text = "Output - Caesar";
+        case 1:
+            outputLabel.text = "Output - Atbash";
+        case 2:
+            outputLabel.text = "Output - Vigenère";
+        default:
+            break
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
